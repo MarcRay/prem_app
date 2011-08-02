5f5ee00515f87dbc4bff5d93d0f3533d651bb827
René <rene@rene-Linux-Ubuntu.(none)>
2011-08-02 13:25:55 -0400

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PremApp::Application.config.secret_token = '93a7620880ed230d93b57a7f76414bceb742af722b4c7b28bc37691cfbb1230010d82349831793da6157d6476d9efc74715ca874fa2ba2179caa81cb4a84b682'
